80e70f19ea2ac69ab583764690427f951eca9e6b
Gavin <gavin@kodekoan.com>
2013-02-27 18:22:51 -0800

    hacked in a couchapp task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
 /*jshint node:true */
 /*globals module:true */
 "use strict";
+var url = require('url');
+
 module.exports = function(grunt) {
 
   // Project configuration.
@@ -22,6 +24,30 @@ module.exports = function(grunt) {
             }
         }
     }
+  });
+
+  /* Push up to couchdb server for dev test */
+  grunt.registerTask('couchapp', "deploy couchapp", function() {
+      var done = this.async();
+
+      var url_options = {
+          protocol: process.env.COUCHAPP_PROTOCOL || 'http',
+          hostname: process.env.COUCHAPP_SERVER || 'localhost',
+          port: process.env.COUCHAPP_PORT || '5984',
+          pathname: process.env.COUCHAPP_PATH || '/acralyzer'
+      };
+      if (process.env.COUCHAPP_AUTH) {
+          url_options.auth = process.env.COUCHAPP_AUTH;
+      }
+
+      grunt.util.spawn({
+          cmd: "couchapp",
+          args: ["push",url.format(url_options)]
+      }, function(err, res, code) {
+          grunt.log.ok();
+          grunt.log.write(res.stderr);
+          done();
+      });
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
